c480d701bc96dcba42d90029cda8193a3fdfe20e
Terngu Emanuel <poporxr@gmail.com>
2026-02-02 17:08:41 +0100

    fixed layout issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} antialiased bg-slate-50 text-slate-800 h-screen overflow-hidden flex`}
+        className={`${inter.variable} antialiased bg-slate-50 text-slate-800 h-screen overflow-hidden flex pb-40`}
       >
         {children}
       </body>
